e881631c07bb8e256b495e894e9c281aa0dfd2ef
Nakura125 <angeloalberico00@gmail.com>
2025-06-11 09:10:12 +0200

    add log file for cron

--- docker-env/set_cron_from_config.sh
+++ docker-env/set_cron_from_config.sh
@@ -13,10 +13,10 @@ HOUR=$(echo $TIME | cut -d: -f1)
 MIN=$(echo $TIME | cut -d: -f2)
 
 if [ "$INTERVAL" = "daily" ]; then
-    CRON="$MIN $HOUR * * * $SCRIPT_PATH"
+    CRON="$MIN $HOUR * * * $SCRIPT_PATH >> /wrf/WRF/cronjob.log 2>&1"
 elif [ "$INTERVAL" = "weekly" ]; then
     # 0 = domenica, puoi cambiare il giorno (0-6)
-    CRON="$MIN $HOUR * * 0 $SCRIPT_PATH"
+    CRON="$MIN $HOUR * * 0 $SCRIPT_PATH >> /wrf/WRF/cronjob.log 2>&1"
 else
     echo "run_interval non valido (usa 'daily' o 'weekly')"
     exit 1
